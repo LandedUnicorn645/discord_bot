823c63b86b0172ca421a9907522e363a1103f7b9
Lorcan <lorcan@lorcan.mcveigh@gmail.com>
2018-07-10 23:13:08 +0100

    Greeting function

--- discord_bot.py
+++ discord_bot.py
@@ -1,3 +1,17 @@
-import discord
-from discord.ext import commands
-import random 
+from discord import Game
+from discord.ext.commands import Bot
+
+BOT_PREFIX = ""
+TOKEN = ""
+
+cleint - Bot(command_prefix=BOT_PREFIX)
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name="your emotions"))
+    print("Logged in as " + client.user.name)
+
+@client.command(name='greet',
+                description='greets the person who mention the bot',)
+async def greet(context):
+    await client.say("Hello " + context.message.author.mention, + ":wave:")
