fb849a3cf68e5e901c199a710df47554135f36be
Lorcan <lorcan@lorcan.mcveigh@gmail.com>
2018-12-21 20:10:11 +0000

    fixed bug with mentioning users

--- discord_bot.py
+++ discord_bot.py
@@ -3,21 +3,24 @@ from discord.ext.commands import Bot
 
 BOT_PREFIX = "?"
 TOKEN = ""
-
 client = Bot(command_prefix=BOT_PREFIX)
 
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="your emotions"))
-    print("Logged in as " + client.user.name)
+    print( "Logged in as " + client.user.name)
+   
 
 @client.command(name='greet',
-                description='greets the person who mention the bot',)
-async def greet(context):
-    await client.say("Hello " + context.message.author.mention, + ":wave:")
+                description='greets the person who mention the bot',pass_context=True)
+async def greet(ctx):
+    
+    msg = "Hello {} :wave:".format(ctx.message.author.mention)
+    await client.say(msg)
+    #await client.say("Hello " + context.message.author.mention, + ":wave:")
 
 @client.command(name='changeWord', description='capitalises every second letter')
-async def changeWord(ctx, arg)
+async def changeWord(arg):
     newword = ""
     count = 0
     for w in arg:
@@ -26,4 +29,8 @@ async def changeWord(ctx, arg)
         else:
             newword += w 
         count += 1
-    await ctx.send(newword)
+    await client.say(newword)
+
+
+client.run(TOKEN)
+    
